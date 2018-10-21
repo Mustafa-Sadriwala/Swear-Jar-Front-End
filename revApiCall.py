9275d88ef772c4824534fdfb5f77084ec46b7646
Evan <ews170030@utdallas.edu>
2018-10-21 09:46:27 -0500

    Checks swears

--- revApiCall.py
+++ revApiCall.py
@@ -1,37 +1,54 @@
 
 import time
-import pandas as pd
 import json
+import re
 
 
 from rev_ai.speechrec import RevSpeechAPI
 
 
 class revSpeechmod:
-    def __init__(self):
-        self.swearWords = []
+    def __init__(self, transcript = ""):
+        if transcript != "":
+            with open(transcript, 'r') as fp:
+                self.transcript = json.load(fp)
+                print(self.transcript)
+        self.swearWords=[]
         self.client = RevSpeechAPI('01DuAIahoMCykRyp2-xV6GC9BqjbtELzeh7GCiQ2DlfMdihYRTui2cTt1D9kOjPZHhrHLEa87RfnFdii-9Cy4v1n3-Mdc')
-    def await_transcript(client, id_):
+    def await_transcript(self, client, id_):
        while client.view_job(id_)['status'] == 'in_progress':
            print('waiting...')
            time.sleep(5)
        return client.get_transcript(id_)
-    def addSwears(x, self):
-        self.swearWords.append(x)
-    def removeSwear(x,self):
+    def addSwears(self, x):
+        for a in x:
+            self.swearWords.append(a.lower())
+    def removeSwear(self, x):
         self.swearWords.remove(x)
     def getTranscript(self, file):
         result = self.client.submit_job_local_file(file)
         transcript = self.await_transcript(self.client, result['id'])
         print(transcript)
         print(transcript['monologues'][0]['elements'][:10])
-        with open('data.json', 'w') as fp:
+        with open(str(file) + ".json", 'w') as fp:
             json.dump(transcript, fp)
         return transcript
-
-    def checkSwears(self, file, transcript = ""):
-        if(transcript == ""):
+    def setTranscript(self, file):
+        self.transcript = file
+    def checkSwears(self):
+        swears = 0;
+        if(self.transcript == ""):
             transcript = self.getTranscript(file)
-        df = pd.Dataframe.from_dict(transcript)
-
-
+        bagOfWords = []
+        x = self.transcript['monologues'][0]['elements']
+        for i in x:
+            if(re.search('[a-zA-Z]', i['value'])!=None):
+                a = i['value'].lower()
+                words = a.split(' ')
+                for i in words:
+                    bagOfWords.append(i)
+        for i in bagOfWords:
+            print(i)
+            if i in self.swearWords:
+                swears +=1
+        print(swears)
